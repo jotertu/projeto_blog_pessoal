5668420e836ccb566726937a6b7231c9991eb815
jotertu <joaotertuliano00@gmail.com>
2023-08-30 16:25:47 -0300

    feat: Adicionar CRUD de postagens em PostagemController e dados simulados em PostagemRepository
    Implementação inicial do CRUD de postagens na PostagemController, incluindo endpoints para criar, ler, atualizar e deletar postagens. Em PostagemRepository, foi adicionada uma classe com dados simulados para representar as postagens.

--- blogPessoal/src/main/java/com/blogPessoal/blogPessoal/model/Postagem.java
+++ blogPessoal/src/main/java/com/blogPessoal/blogPessoal/model/Postagem.java
@@ -25,7 +25,7 @@ public class Postagem {
 	private String titulo;
 
 	@NotBlank(message = "O atributo texto é Obrigatório!")
-	@Size(min = 5, max = 100, message = "O atributo texto deve conter no mínimo 05 e no máximo 1000 caracteres")
+	@Size(min = 5, max = 1000, message = "O atributo texto deve conter no mínimo 05 e no máximo 1000 caracteres")
 	private String texto;
 	@UpdateTimestamp
 	private LocalDateTime data;
--- blogPessoal/src/main/java/com/blogPessoal/blogPessoal/repository/PostagemRepository.java
+++ blogPessoal/src/main/java/com/blogPessoal/blogPessoal/repository/PostagemRepository.java
@@ -1,8 +1,15 @@
 package com.blogPessoal.blogPessoal.repository;
 
+import  java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.blogPessoal.blogPessoal.model.Postagem;
+
+import org.springframework.data.repository.query.Param;
+
 public interface PostagemRepository extends JpaRepository <Postagem,Long> {
+
+    public List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
 
 }
