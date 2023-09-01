07a481dcc864b0f880602fd32530f59998d4ab57
jotertu <joaotertuliano00@gmail.com>
2023-08-31 23:10:05 -0300

    docs: Adicionar comentários explicativos para funções críticas em PostagemController

--- blogPessoal/src/main/java/com/blogPessoal/blogPessoal/model/Postagem.java
+++ blogPessoal/src/main/java/com/blogPessoal/blogPessoal/model/Postagem.java
@@ -11,40 +11,6 @@ import org.hibernate.annotations.UpdateTimestamp;
 /**
  * Camada responsável pela abstração dos nossos Objetos em registros das nossas tabelas, que serão geradas no Banco de
  * dados. As Classes criadas nesta camada representam os objetos que serão persistidos no Banco de dados.
- *
- * ALERTA DE BSM: Mantenha a Atenção aos Detalhes ao criar a Camada Model. Um erro muito comum é criar o pacote
- * na Source Folder de Testes, ao invés de criar na Source Folder Principal.
- *
- * Observe que acima de cada Atributo foram adicionadas algumas Anotações. Estas anotações têm a função de configurar os parâmetros do
- * Banco de dados e criar validações para os dados que serão inseridos no objeto (tamanho, formato e etc).
- *
- *		Estratégias para geração da Chave Primária:
- *
- * 		GenerationType.AUTO
- * 		Valor padrão, deixa com o provedor de persistência a escolha da estratégia mais adequada de acordo com
- * 		o Banco de dados.
- *
- * 		GenerationType.IDENTITY
- * 		Informamos ao provedor de persistência que os valores a serem atribuídos ao identificador único serão
- * 		gerados pela coluna de auto incremento do banco de dados. Assim, um valor para o identificador é gerado
- * 		para cada registro inserido no banco. Alguns bancos de dados podem não suportar essa opção.
- *
- * 		GenerationType.SEQUENCE
- * 		Informamos ao provedor de persistência que os valores serão gerados a partir de uma sequence. Caso não
- * 		seja especificado um nome para a sequence, será utilizada uma sequence padrão, a qual será global, para
- * 		todas as entidades. Caso uma sequence seja especificada, o provedor passará a adotar essa sequence para
- * 		criação das chaves primárias. Alguns bancos de dados podem não suportar essa opção, como o MySQL por
- * 		exemplo.
- *
- * 		GenerationType.TABLE
- * 		Com a opção TABLE é necessário criar uma tabela para gerenciar as chaves primárias. Por causa da
- * 		sobrecarga de consultas necessárias para manter a tabela atualizada, essa opção é pouco recomendada.
- *
- * DICA: Para relembrar os tipos de dados em Java, clique aqui e explore os principais tipos de dados e Classes que o Java oferece.
- * LINK: https://github.com/rafaelq80/spring_content/blob/main/03_spring/java_tipos.md
- *
- * DICA: Acesse o Guia do JPA e explore outras opções de Validação para os Atributos. Essas validações serão muito úteis em seus projetos futuros.
- * LINK: https://github.com/rafaelq80/spring_content/blob/main/03_spring/guia_jpa.md
  * */
 
 /*
@@ -52,13 +18,6 @@ import org.hibernate.annotations.UpdateTimestamp;
  * Banco de dados da aplicação.
  * */
 @Entity
-
-/*
- * A Anotação @Table indica o nome da Tabela no Banco de dados. Caso esta anotação não seja declarada, o Banco de dados criará a
- * tabela com o mesmo nome da Classe Model (Postagem). Observe que o nome da Tabela segue o padrão tb_nome-da-tabela (tb_postagens). O
- * prefixo tb indica que se trata de uma Table (Tabela). O nome da Tabela é recomendado que seja o mesmo da Classe Model (postagem), em
- * letras minúsculas, sem espaços em branco ou caracteres especiais e acentos.
- * */
 @Table(name = "tb_postagens")
 public class Postagem {
 
@@ -67,31 +26,22 @@ public class Postagem {
 	// A Anotação @Id inidica que o Atributo anotado será a Chave Primária (Primary Key - PK) da Tabela tb_postagens.
 	@Id
 
-	/*
-	 * A Anotação @GeneratedValue indica que a Chave Primária será gerada pelo Spring Data JPA. O parâmetro strategy indica de que
-	 * forma esta Chave Primária será gerada. A Estratégia GenerationType.IDENTITY indica que a Chave Primária será gerada pelo Banco de dados
-	 * através da opção auto-incremento (auto-increment), que gera uma sequência numérica iniciando em 1. Veja outras estratégias de geração da
-	 * Chave Primária
-	 *
-	 * ATENÇÃO: Não confundir o auto-incremento do Banco de Dados que inicia em 1 com o índice de um Array (Vetor ou Matriz) que inicia em 0.
-	 * */
+	// A Anotação @GeneratedValue indica que a Chave Primária será gerada pelo Spring Data JPA.
+	// O parâmetro strategy indica de que forma esta Chave Primária será gerada.
+	// A Estratégia GenerationType.IDENTITY indica que a Chave Primária será gerada pelo Banco de dados
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id; // Atributo: ID; Tipo de dado Java: Long; Tipo de dado MySQL: BIGINT;
 
-	// Título da postagem
+	// Identificador único da tabela
+	private Long id; // Atributo: ID; Tipo de dado Java: Long; Tipo de dado MySQL: BIGINT;
 
 	// A anotação @NotBlank não permite que o Atributo seja Nulo ou contenha apenas Espaços em branco. Você pode
 	// configurar uma mensagem para o usuário através do Atributo message.
 	@NotBlank(message = "O atributo título é Obrigatório!")
 
-	/*
-	 * A anotação @Size define o valor Mínimo (min) e Máximo (max) de Caracteres do Atributo. Não é obrigatório configurar os
-	 * 2 parâmetros. Como o parâmetro max foi configurado, observe que o mesmo valor informado será inserido na definição dos Atributos título
-	 * (varchar(100)) e texto (varchar(1000)) na tabela tb_postagens no Banco de dados. Você pode configurar uma mensagem para o usuário através
-	 * do Atributo message.
-	 *
-	 * */
+	 // A anotação @Size define o valor Mínimo (min) e Máximo (max) de Caracteres do Atributo.
 	@Size(min = 5, max = 100, message = "O atributo título deve conter no mínimo 05 e no máximo 100 caracteres")
+
+	// Título da postagem
 	private String titulo; // Atributo: título; Tipo de dado Java: String; Tipo de dado MySQL: VARCHAR(100);
 
 	// Conteúdo da postagem
@@ -100,11 +50,7 @@ public class Postagem {
 	private String texto; // Atributo: texto; Tipo de dado Java: String; Tipo de dado MySQL: VARCHAR(1000);
 
 	// Data e hora da publicação/atualização da postagem
-
-	/*
-	 * A anotação @UpdateTimestamp configura o Atributo data como Timestamp, ou seja, o Spring se encarregará de obter a data do
-	 * Sistema Operacional e inserir no Atributo data toda vez que um Objeto da Classe Postagem for criado ou atualizado.
-	 * */
+	// A anotação @UpdateTimestamp configura o Atributo data como Timestamp
 	@UpdateTimestamp
 	private LocalDateTime data; // Atributo: data; Tipo de dado Java: LocalDateTime; Tipo de dado MySQL: DATETIME(6);
 
--- blogPessoal/src/main/java/com/blogPessoal/blogPessoal/repository/PostagemRepository.java
+++ blogPessoal/src/main/java/com/blogPessoal/blogPessoal/repository/PostagemRepository.java
@@ -18,52 +18,11 @@ import org.springframework.stereotype.Repository;
  * abstratos. E como tal, obviamente não pode ser instanciada, ou seja, ela só contém as declarações dos Métodos e constantes, nenhuma
  * implementação, apenas as assinaturas dos Métodos, que serão implementados em uma Classe.
  *
- *      Métodos Padrão da Interface JpaRepository:
- *
- *      save(Objeto objeto)
- * 	    Cria ou Atualiza um objeto no Banco de Dados.
- *
- * 	    findById(Long id)
- * 	    Retorna (exibe) um Objeto persistido de acordo com o id informado.
- *
- * 	    existsById(Long id)
- * 	    Retorna True se um Objeto identificado pelo id estiver persistido no Banco de dados.
- *
- * 	    findAll()
- * 	    Retorna (exibe) todos os Objetos persistidos.
- *
- * 	    deletedById(Long id)
- * 	    Localiza um Objeto persistido pelo id e deleta caso ele seja encontrado. Não é possível
- * 	    desfazer esta operação.
- *
- * 	    deleteAll()
- * 	    Deleta todos os Objetos persistidos.Não é possível desfazer esta operação.
  * */
 
-/*
- * A Anotação (Annotation) @Repository indica que a Interface é do tipo repositório, ou seja, ela é responsável pela interação com o
- * Banco de dados através dos Métodos padrão (Herdados da Interface JPA Repository) e das Query Methods, que são Métodos personalizados
- * que geram consultas (Instruções SQL do tipo Select), através da combinação de palavras chave, que representam os comandos da linguagem
- * SQL.
- * */
+// A Anotação (Annotation) @Repository indica que a Interface é do tipo repositório
 @Repository
 
-/*
- * Observe que na declaração da Interface foi adicionada a Herança através da palavra reservada extends com a Interface JpaRepository,
- * que recebe 2 parâmetros:
- *
- * 1 - A Classe Postagem, que é a Entidade que será mapeada em nosso Banco de dados (Lembre-se que a Classe Postagem foi quem gerou a
- * nossa tabela tb_postagens)
- *
- * 2 - O Long representa a nossa Chave Primária (Primary Key), que é o Atributo que recebeu a anotação @Id na nossa Classe Postagem (o
- * Atributo também se chama id em nossa Classe Postagem).
- *
- * Estes 2 parâmetros são do tipo Java Generics (podem receber qualquer tipo de Objeto <T, T>). Dentro contexto do JPA são o mínimo
- * necessário para executar os Métodos padrão da Interface Repository, que serão implementados na próxima etapa na Classe
- * PostagemController. Estes Métodos básicos já ficam automaticamente disponíveis no Recurso Postagem a partir do momento que a Interface
- * PostagemRepository herda a Interface JpaRepository.
- *
- * */
 public interface PostagemRepository extends JpaRepository <Postagem,Long> {
 
     public List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
